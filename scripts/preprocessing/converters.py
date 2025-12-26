2428145efd105aea4922c4b3deed5705263f7c0c
Sheehan Rahman <sheehanrahman06@gmail.com>
2025-12-25 20:24:49 -0500

    Add check for empty image in dcm_to_png function

--- scripts/preprocessing/converters.py
+++ scripts/preprocessing/converters.py
@@ -9,6 +9,11 @@ def dcm_to_png(dcm_path, target_size=(256, 256)):
 
     original_pixels = dcm.pixel_array.astype(float)  # convert int to float
     normalized_pixels = original_pixels - np.min(original_pixels)  # range starts at 0
+
+    # check for empty image before dividing
+    if np.max(normalized_pixels) == 0:
+        return None
+
     normalized_pixels = normalized_pixels / np.max(normalized_pixels)  # range is 0 to 1
     normalized_pixels = (normalized_pixels * 255).astype(np.uint8)  # range is 0 to 255
 

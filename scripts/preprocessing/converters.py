6af2e72621e7d61b36c85eeb5051704737ea0880
Sheehan Rahman <sheehanrahman06@gmail.com>
2025-12-25 22:12:30 -0500

    Update dcm_to_png function to deal with inverted images

--- scripts/preprocessing/converters.py
+++ scripts/preprocessing/converters.py
@@ -6,8 +6,12 @@ from PIL import Image
 # normalizes DICOM then converts to PNG in target size
 def dcm_to_png(dcm_path, target_size=(256, 256)):
     dcm = pydicom.dcmread(dcm_path)
-
     original_pixels = dcm.pixel_array.astype(float)  # convert int to float
+
+    # fixes inverted images
+    if dcm.PhotometricInterpretation == "MONOCHROME1":
+        original_pixels = np.max(original_pixels) - original_pixels
+
     normalized_pixels = original_pixels - np.min(original_pixels)  # range starts at 0
 
     # check for empty image before dividing

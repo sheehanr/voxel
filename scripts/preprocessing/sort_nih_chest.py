342c3a621651e52b937d76ebbbd153bd85d4e1c2
Sheehan Rahman <sheehanrahman06@gmail.com>
2025-12-28 17:15:42 -0500

    Add directory paths and create_dirs function

---
+++ scripts/preprocessing/sort_nih_chest.py
@@ -0,0 +1,18 @@
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, "../../data"))
+
+UNSORTED_DIR = os.path.join(DATA_DIR, "unsorted/nih_chest/images")
+TRAIN_DIR = os.path.join(DATA_DIR, "unsorted/nih_chest/sorted_train")
+TEST_DIR = os.path.join(DATA_DIR, "unsorted/nih_chest/sorted_test")
+TRAIN_FILE = os.path.join(DATA_DIR, "unsorted/nih_chest/train_val_list.txt")
+TEST_FILE = os.path.join(DATA_DIR, "unsorted/nih_chest/test_list.txt")
+
+TARGET_DIMENSIONS = (256, 256)
+
+
+# creates needed dirs if not created
+def create_dirs():
+    os.makedirs(TRAIN_DIR, exist_ok=True)
+    os.makedirs(TEST_DIR, exist_ok=True)

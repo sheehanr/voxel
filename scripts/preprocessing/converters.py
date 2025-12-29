0a253327448b85ab20387707c3fee46d7f0d882d
Sheehan Rahman <sheehanrahman06@gmail.com>
2025-12-29 17:47:00 -0500

    Adjust comments for clarity

--- scripts/preprocessing/converters.py
+++ scripts/preprocessing/converters.py
@@ -20,15 +20,17 @@ def dcm_to_png(dcm_path, target_size=(256, 256)):
     if dcm.PhotometricInterpretation == "MONOCHROME1":
         original_pixels = np.max(original_pixels) - original_pixels
 
-    normalized_pixels = original_pixels - np.min(original_pixels)  # range starts at 0
+    # adjust range so it starts from 0
+    normalized_pixels = original_pixels - np.min(original_pixels)
 
     # check for empty image before dividing
     if np.max(normalized_pixels) == 0:
         return None
 
-    normalized_pixels = normalized_pixels / np.max(normalized_pixels)  # range is 0 to 1
-    normalized_pixels = (normalized_pixels * 255).astype(np.uint8)  # range is 0 to 255
+    normalized_pixels = normalized_pixels / np.max(normalized_pixels)  # adjust range: 0 to 1
+    normalized_pixels = (normalized_pixels * 255).astype(np.uint8)  # adjust range: 0 to 255
 
+    # convert to grayscale and resize
     png = Image.fromarray(normalized_pixels, mode="L")
     png = png.resize(target_size)
 

7fae3de5bca493d58ae74c489df460f2f7334b63
Sheehan Rahman <sheehanrahman06@gmail.com>
2025-12-30 22:44:59 -0500

    Copy rest of sort_OT_xr_heel.py logic and adjust as needed

--- scripts/preprocessing/sort_MD_xr_knee.py
+++ scripts/preprocessing/sort_MD_xr_knee.py
@@ -1,4 +1,8 @@
 import os
+import random
+
+from converters import standardize_image
+from tqdm import tqdm
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, "../../data"))
@@ -45,8 +49,53 @@ def setup_directories():
         return SORTED_TRAIN_DIR, SORTED_VAL_DIR
 
 
+# return list of all image paths
+def list_filepaths():
+    all_filepaths = []
+    for subdir in ["0Normal", "1Doubtful", "2Mild", "3Moderate", "4Severe"]:
+        subdir_path = os.path.join(UNSORTED_DIR, subdir)
+        for filename in os.listdir(subdir_path):
+            if filename.startswith("."):
+                continue
+
+            all_filepaths.append(os.path.join(subdir_path, filename))
+
+    return all_filepaths
+
+
+# randomly separate files into train and val
+def split_dataset(all_filepaths, split_ratio):
+    random.shuffle(all_filepaths)
+    train_len = int(len(all_filepaths) * split_ratio)
+
+    train_filepaths = all_filepaths[:train_len]
+    val_filepaths = all_filepaths[train_len:]
+
+    return train_filepaths, val_filepaths
+
+
+# standardize images and save to correct directory
+def process_files(filepaths, destination):
+    for filepath in tqdm(filepaths):
+        if not os.path.exists(filepath):
+            continue
+
+        img = standardize_image(filepath, TARGET_DIMENSIONS)
+        filename = os.path.basename(filepath)
+        save_path = os.path.join(destination, filename)
+
+        img.save(save_path)
+
+
 def main():
-    pass
+    random.seed(42)
+
+    target_train_dir, target_val_dir = setup_directories()
+    all_filepaths = list_filepaths()
+    selected_train, selected_val = split_dataset(all_filepaths, TRAIN_SAMPLE_PERCENT)
+
+    process_files(selected_train, target_train_dir)
+    process_files(selected_val, target_val_dir)
 
 
 if __name__ == "__main__":

b081b854a09ec1cd89111844a0c705cfb1ec4990
Sheehan Rahman <sheehanrahman06@gmail.com>
2025-12-30 19:01:35 -0500

    Fix formatting and adjust to ensure proper function with original dataset structure

--- scripts/preprocessing/sort_unifesp.py
+++ scripts/preprocessing/sort_unifesp.py
@@ -4,56 +4,63 @@ import pandas as pd
 from converters import dcm_to_png
 from tqdm import tqdm
 
+# note: test folder is not included because it is unlabeled in dataset
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, "../../data"))
+TRAIN_DIR = os.path.join(DATA_DIR, "train")
 
-# note that the test folder is not included because it is unlabeled
-UNSORTED_DIR = os.path.join(DATA_DIR, "unsorted/unifesp/train")
-SORTED_DIR = os.path.join(DATA_DIR, "unsorted/unifesp/sorted_train")
-MULTI_DIR = os.path.join(SORTED_DIR, "multi_target")
-CSV_FILE = os.path.join(DATA_DIR, "unsorted/unifesp/train.csv")
+DATASET_DIR = os.path.join(DATA_DIR, "downloads/UNIFESP_xr_fullbody")
+UNSORTED_TRAIN_DIR = os.path.join(DATASET_DIR, "train")
+
+# to enable manual review
+SORTED_TRAIN_DIR = os.path.join(TRAIN_DIR, "unifesp")
+MULTI_TARGET_DIR = os.path.join(SORTED_TRAIN_DIR, "xr_multi_target")
+
+CSV_FILE = os.path.join(DATASET_DIR, "train.csv")
 
 TARGET_DIMENSIONS = (256, 256)
 
-# desired dir names
+# pytorch class names
 ANATOMICAL_REGIONS = {
-    0: "xr_abdomen_unifesp",
-    1: "xr_ankle_unifesp",
-    2: "xr_cervical_spine_unifesp",
-    3: "xr_chest_unifesp",
-    4: "xr_clavicles_unifesp",
-    5: "xr_elbow_unifesp",
-    6: "xr_feet_unifesp",
-    7: "xr_finger_unifesp",
-    8: "xr_forearm_unifesp",
-    9: "xr_hand_unifesp",
-    10: "xr_hip_unifesp",
-    11: "xr_knee_unifesp",
-    12: "xr_lower_leg_unifesp",
-    13: "xr_lumbar_spine_unifesp",
-    14: "xr_others_unifesp",
-    15: "xr_pelvis_unifesp",
-    16: "xr_shoulder_unifesp",
-    17: "xr_sinus_unifesp",
-    18: "xr_skull_unifesp",
-    19: "xr_thigh_unifesp",
-    20: "xr_thoracic_spine_unifesp",
-    21: "xr_wrist_unifesp",
+    0: "xr_abdomen",
+    1: "xr_ankle",
+    2: "xr_cervical_spine",
+    3: "xr_chest",
+    4: "xr_clavicles",
+    5: "xr_elbow",
+    6: "xr_feet",
+    7: "xr_finger",
+    8: "xr_forearm",
+    9: "xr_hand",
+    10: "xr_hip",
+    11: "xr_knee",
+    12: "xr_lower_leg",
+    13: "xr_lumbar_spine",
+    14: "xr_others",
+    15: "xr_pelvis",
+    16: "xr_shoulder",
+    17: "xr_sinus",
+    18: "xr_skull",
+    19: "xr_thigh",
+    20: "xr_thoracic_spine",
+    21: "xr_wrist",
 }
 
 
 # create directories if needed
 def setup_directories():
-    os.makedirs(SORTED_DIR, exist_ok=True)
-    os.makedirs(MULTI_DIR, exist_ok=True)
-    for region in ANATOMICAL_REGIONS.values():  # subdir for each body part
-        os.makedirs(os.path.join(SORTED_DIR, region), exist_ok=True)
+    os.makedirs(SORTED_TRAIN_DIR, exist_ok=True)
+    os.makedirs(MULTI_TARGET_DIR, exist_ok=True)
+
+    # create subdirectory for each body part
+    for region in ANATOMICAL_REGIONS.values():
+        os.makedirs(os.path.join(SORTED_TRAIN_DIR, region), exist_ok=True)
 
 
 # map each image id to its full path
 def create_img_map():
     img_map = {}
-    for root, dir_names, filenames in os.walk(UNSORTED_DIR):
+    for root, dir_names, filenames in os.walk(UNSORTED_TRAIN_DIR):
         for f in filenames:
             if f.endswith(".dcm"):
                 img_id = f.split("-")[0]
@@ -82,19 +89,18 @@ def process_files(img_map):
         target_str = str(row_data["Target"]).strip()
         targets = target_str.split(" ")
         if len(targets) > 1:  # multiple anatomical regions
-            save_path = os.path.join(MULTI_DIR, f"{'_'.join(targets)}_{short_id}.png")
+            save_path = os.path.join(MULTI_TARGET_DIR, f"{'_'.join(targets)}_{short_id}.png")
             png_img.save(save_path)
         else:
             target = int(targets[0])
             save_dir = ANATOMICAL_REGIONS[target]
-            save_path = os.path.join(SORTED_DIR, save_dir, f"{short_id}.png")
+            save_path = os.path.join(SORTED_TRAIN_DIR, save_dir, f"{short_id}.png")
             png_img.save(save_path)
 
 
 def main():
     setup_directories()
     img_map = create_img_map()
-
     process_files(img_map)
 
 

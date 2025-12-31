b71047960bff3a7ee3a83ad97819c6907b3fc164
Sheehan Rahman <sheehanrahman06@gmail.com>
2025-12-30 20:34:19 -0500

    Update constant variables and setup_directories to work with original dataset structure

--- scripts/preprocessing/sort_heel.py
+++ scripts/preprocessing/sort_heel.py
@@ -6,16 +6,15 @@ from tqdm import tqdm
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, "../../data"))
+TRAIN_DIR = os.path.join(DATA_DIR, "train")
+VAL_DIR = os.path.join(DATA_DIR, "val")
 
-UNSORTED_DIR = os.path.join(DATA_DIR, "unsorted/heel_dataset")
+DATASET_DIR = os.path.join(DATA_DIR, "downloads/OT_xr_heel")
+UNSORTED_DIR = os.path.join(DATASET_DIR, "all_category/all_category")
 
 # pytorch class directories
-TRAIN_DIR = os.path.join(DATA_DIR, "train/xr_heel")
-VAL_DIR = os.path.join(DATA_DIR, "val/xr_heel")
-
-# in case class directories are not empty
-BACKUP_TRAIN_DIR = os.path.join(DATA_DIR, "unsorted/heel_dataset/sorted_train")
-BACKUP_VAL_DIR = os.path.join(DATA_DIR, "unsorted/heel_dataset/sorted_val")
+SORTED_TRAIN_DIR = os.path.join(TRAIN_DIR, "xr_heel")
+SORTED_VAL_DIR = os.path.join(VAL_DIR, "xr_heel")
 
 TRAIN_SAMPLE_PERCENT = 0.9
 TARGET_DIMENSIONS = (256, 256)
@@ -23,25 +22,33 @@ TARGET_DIMENSIONS = (256, 256)
 
 # return paths of target directories
 def setup_directories():
-    os.makedirs(TRAIN_DIR, exist_ok=True)
-    os.makedirs(VAL_DIR, exist_ok=True)
+    os.makedirs(SORTED_TRAIN_DIR, exist_ok=True)
+    os.makedirs(SORTED_VAL_DIR, exist_ok=True)
+
+    target_train_dir = SORTED_TRAIN_DIR
+    target_val_dir = SORTED_VAL_DIR
 
     # create backups if needed
-    if len(os.listdir(TRAIN_DIR)) > 0 or len(os.listdir(VAL_DIR)) > 0:
-        print("ERROR: Primary train and/or val directory not empty, files will be stored in backup directories")
+    if len(os.listdir(SORTED_TRAIN_DIR)) > 0 or len(os.listdir(SORTED_VAL_DIR)) > 0:
+        print("WARNING: data/train/xr_heel or data/val/xr_heel are not empty. What would you like to do?:")
+        print("\t1. Continue with placing the images in those folders")
+        print("\t2. Place the images in data/[...]/xr_heel/xr_heel_OT")
+        print("\t(Note: option 2 requires manual review and transfer of files into xr_chest before training)")
+        choice = input("Enter 1 or 2: ")
 
-        os.makedirs(BACKUP_TRAIN_DIR, exist_ok=True)
-        os.makedirs(BACKUP_VAL_DIR, exist_ok=True)
+        if choice != "1":
+            target_train_dir = os.path.join(SORTED_TRAIN_DIR, "xr_heel_OT")
+            target_val_dir = os.path.join(SORTED_VAL_DIR, "xr_heel_OT")
 
-        return BACKUP_TRAIN_DIR, BACKUP_VAL_DIR
+            os.makedirs(target_train_dir, exist_ok=True)
+            os.makedirs(target_val_dir, exist_ok=True)
 
-    return TRAIN_DIR, VAL_DIR
-
+    return target_train_dir, target_val_dir
 
 # return list of all image paths
 def list_filepaths():
     all_filepaths = []
-    for subdir in ["heelspur", "normal", "severe"]:
+    for subdir in ["heelspur", "normal", "sever"]:
         subdir_path = os.path.join(UNSORTED_DIR, subdir)
         for filename in os.listdir(subdir_path):
             if filename.startswith("."):

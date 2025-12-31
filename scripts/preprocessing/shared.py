10edb13a50e42a2f3830d442283f4a61d735da68
Sheehan Rahman <sheehanrahman06@gmail.com>
2025-12-31 01:48:18 -0500

    Create script for shared functions to minimize repeated code in sorters

---
+++ scripts/preprocessing/shared.py
@@ -0,0 +1,56 @@
+import os
+import random
+
+
+# return paths of target directories
+def setup_directories(pytorch_class, class_subdir, sorted_train_dir, sorted_val_dir):
+    os.makedirs(sorted_train_dir, exist_ok=True)
+    os.makedirs(sorted_val_dir, exist_ok=True)
+
+    print("Where should images be placed?:")
+    print(f"    1. Directly in data/train/{pytorch_class}")
+    print(f"    2. In subdirectory data/train/{pytorch_class}/{class_subdir} (recommended)")
+    print("    (Note: option 2 requires manual review and transfer before training)")
+    choice = input("Enter 1 or 2: ")
+    print("")
+
+    if choice != "1":
+        target_train_dir = os.path.join(sorted_train_dir, class_subdir)
+        target_val_dir = os.path.join(sorted_val_dir, class_subdir)
+
+        os.makedirs(target_train_dir, exist_ok=True)
+        os.makedirs(target_val_dir, exist_ok=True)
+
+        return target_train_dir, target_val_dir
+
+    else:
+        return sorted_train_dir, sorted_val_dir
+
+
+# returns list of all image paths
+def list_filepaths(unsorted_dir, subdirs_list):
+    all_filepaths = []
+    for subdir in subdirs_list:
+        subdir_path = os.path.join(unsorted_dir, subdir)
+        for filename in os.listdir(subdir_path):
+            if filename.startswith("."):
+                continue
+
+            all_filepaths.append(os.path.join(subdir_path, filename))
+
+    return all_filepaths
+
+
+# randomly separate files into train and val
+def split_dataset(all_filepaths, split_ratio=0.9):
+    random.shuffle(all_filepaths)
+    train_len = int(len(all_filepaths) * split_ratio)
+
+    train_filepaths = all_filepaths[:train_len]
+    val_filepaths = all_filepaths[train_len:]
+
+    return train_filepaths, val_filepaths
+
+
+def get_subdirectories(unsorted_dir):
+    return [d for d in os.listdir(unsorted_dir) if os.path.isdir(os.path.join(unsorted_dir, d))]

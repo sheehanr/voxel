ca3fc56d0a54bc46254dc2eb95ccef6802da6bfb
Sheehan Rahman <sheehanrahman06@gmail.com>
2025-12-31 20:52:05 -0500

    Update constant variables

--- scripts/preprocessing/sort_AIMI_xr_lower.py
+++ scripts/preprocessing/sort_AIMI_xr_lower.py
@@ -1,6 +1,7 @@
 import os
 
 DATASET_NAME = "AIMI_xr_lower"
+SUFFIX = "_AIMI"
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, "../../data"))
@@ -11,9 +12,9 @@ SRC_DIR = DATASET_DIR
 
 TRAIN_CSV = os.path.join(DATASET_DIR, "labels.csv")
 
-TRAIN_DST = os.path.join(TRAIN_DIR, "xr_lower_AIMI")
+TRAIN_DST = os.path.join(TRAIN_DIR, "xr_AIMI")
 
-CLASS_NAMES = {
+CLASS_MAP = {
     "XR KNEE": "xr_knee",
     "XR HIP": "xr_hip",
     "XR ANKLE": "xr_ankle",

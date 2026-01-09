76fb4949e28f7df22a4b39b88aa6f04c659911c5
Sheehan Rahman <sheehanrahman06@gmail.com>
2026-01-08 20:58:56 -0500

    Rename .dir to .setup

--- scripts/preprocessing/utils/__init__.py
+++ scripts/preprocessing/utils/__init__.py
@@ -1,8 +1,8 @@
 from .data import sample_files, split_data
-from .dir import init_multi_dirs, init_single_dir, multi_dst_prompt, single_dst_prompt
 from .image import inversion_helper, load_dcm, normalize_pixels, standardize_pil
 from .io import get_filepaths, get_subdirs, map_files, read_text_file
 from .process import get_dst_path, handle_file, process_class_files, process_classes, process_image
+from .setup import init_multi_dirs, init_single_dir, multi_dst_prompt, single_dst_prompt
 
 __all__ = [
     "sample_files",

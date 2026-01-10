66d6460f63acb161e135a0fb5a4b17d63701e7eb
Sheehan Rahman <sheehanrahman06@gmail.com>
2026-01-10 17:54:57 -0500

    Set up main

--- scripts/preprocessing/sorters/mr_knee_AIMI.py
+++ scripts/preprocessing/sorters/mr_knee_AIMI.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from scripts.preprocessing.utils import get_filepaths, get_subdirs, init_single_dir
+
 DATASET_NAME = "mr_knee_AIMI"
 CLASS_NAME = "mr_knee"
 SUFFIX = "_AIMI"
@@ -12,9 +14,18 @@ VAL_DIR = DATA_DIR / "val"
 DATASET_DIR = DATA_DIR / "downloads" / DATASET_NAME
 SRC_DIR = DATASET_DIR / "MRNet-v1.0"
 
+TRAIN_SRC = SRC_DIR / "train"
+VAL_SRC = SRC_DIR / "val"
+
 
 def main():
-    pass
+    train_dst, val_dst = init_single_dir(CLASS_NAME, TRAIN_DIR, VAL_DIR, SUFFIX)
+
+    train_subdirs = get_subdirs(TRAIN_SRC)
+    val_subdirs = get_subdirs(VAL_SRC)
+
+    train_filepaths = get_filepaths(TRAIN_SRC, train_subdirs)
+    val_filepaths = get_filepaths(VAL_SRC, val_subdirs)
 
 
 if __name__ == "__main__":
